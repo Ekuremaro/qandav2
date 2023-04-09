cf517434b00990cf1f828b63ed9be3fd3cdb29ea
ekuremaro <engrekuremaro@gmail.com>
2023-04-09 20:03:47 +0100

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,20 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
+
+import { Header } from './Header';
+import { HomePage } from './HomePage';
+
+import { fontFamily, fontSize, gray2 } from './Styles';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 import { NotFoundPage } from './NotFoundPage';
+import { QuestionPage } from './QuestionPage';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { AskPage } from "./AskPage";
-
-import { SearchPage } from "./SearchPage";
-
-import { SignInPage } from "./SignInPage";
-
-import { Header } from "./Header";
-import { HomePage } from "./HomePage";
-
-import { fontFamily, fontSize, gray2 } from "./Styles";
+import React from 'react';
+const AskPage = React.lazy(() => import('./AskPage'));
 
 function App() {
   return (
@@ -28,13 +29,29 @@ function App() {
         <Header />
         <Routes>
           <Route path="" element={<HomePage />} />
-
           <Route path="search" element={<SearchPage />} />
-
-          <Route path="ask" element={<AskPage />} />
-
+          <Route
+            path="ask"
+            element={
+              <React.Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </React.Suspense>
+            }
+          />
           <Route path="signin" element={<SignInPage />} />
-          <Route path="*" element={<NotFoundPage/>} />
+          <Route path="questions/:questionId" element={<QuestionPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
     </BrowserRouter>
